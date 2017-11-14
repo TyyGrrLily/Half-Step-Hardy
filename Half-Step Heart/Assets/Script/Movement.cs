84843d25a024fc47420c2da8f08133f70843e175
Izzy Hardy <hardy.izabel@gmail.com>
2017-11-13 16:15:32 -0800

    Initial Push, Here we go~

---
+++ Half-Step Heart/Assets/Script/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.parent.position += new Vector3(.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.parent.position -= new Vector3(.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.parent.position += new Vector3(0, .1f, 0);
+        }
+    }
+}
